3f590d0b8348e3a4e03354a8f8acb1ccaf757014
Dhruv Bhanushali <dhruv_b@live.com>
2016-12-30 04:25:34 +0530

    Write code that manages the navigation between various pages

---
+++ js/script.js
@@ -0,0 +1,45 @@
+var content, heading;
+
+function addClass(element, className) {
+    element.className += (' ' + className);
+}
+
+function removeClass(element, className) {
+    element.className = element.className.replace((' ' + className), '');
+}
+
+function loadPage(url) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', 'html/' + url + '.html', true);
+    xhr.onreadystatechange = function () {
+        if (this.readyState !== 4) return;
+        if (this.status !== 200) return;
+        content.innerHTML = this.responseText;
+        window.location.hash = url;
+
+        var activeItems = document.getElementsByClassName('active');
+        if (activeItems.length != 0) {
+            var activeItem = activeItems[0];
+            removeClass(activeItem, 'active');
+        }
+        var selectedItem = document.getElementById(url);
+        addClass(selectedItem, 'active');
+    };
+    xhr.send();
+}
+
+function updatePage() {
+    var locationHash = window.location.hash;
+    if (locationHash === '') {
+        loadPage('index');
+    } else {
+        loadPage(locationHash.replace('#', ''));
+    }
+}
+
+window.onload = function () {
+    content = document.getElementById('content');
+    heading = document.getElementsByTagName('h1')[0];
+    content.style.width = heading.offsetWidth + "px";
+    updatePage();
+};
