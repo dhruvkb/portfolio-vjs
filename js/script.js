6de6ce55af139d9f612f00d73324f013e95b8fa6
Dhruv Bhanushali <dhruv_b@live.com>
2017-05-19 04:48:23 +0530

    Rewrite the entire command processing logic using tree, so that all sorts of paths (even those intentionally going in and out with ..) and names can be processed logically

--- js/script.js
+++ js/script.js
@@ -1,232 +1,328 @@
-var $body = $('body');
-var $lastPrompt;
+let $body = $('body');
+let $lastPrompt;
 
-var commandHeirarchy = {
-    'home': [
-        'ls',
-        'help',
-        'clear',
-        'exit',
-        'cd info',
-        'cd résumé',
-        'cd resume',
-        'ccat contact.md',
-        'cat contact.md',
-        'ccat credits.md',
-        'cat credits.md'
-    ],
-    'info': [
-        'cd ..',
-        'ccat info.md',
-        'cat info.md'
-    ],
-    'résumé': [
-        'cd ..',
-        'ccat resume_master.pdf',
-        'ccat resume_computing.pdf',
-        'ccat resume_physics.pdf',
-        'ccat résumé_master.pdf',
-        'ccat résumé_computing.pdf',
-        'ccat résumé_physics.pdf'
-    ]
-};
-commandHeirarchy['home'].forEach(function (homeCommand) {
-    homeCommand = homeCommand.replace(' ', ' ../');
-    commandHeirarchy['info'].push(homeCommand);
-    commandHeirarchy['résumé'].push(homeCommand);
-});
-commandHeirarchy['home'].push('ccat résumé/résumé_master.pdf');
+let basicTree;
+let tree;
+let currentPointer;
+let currentNode;
+
+const TYPE_SPEED = 50;
 
 $(document).ready(function () {
+    // Set up the filesystem structure
+    getStructure();
+    generateTree();
+    currentNode = tree.root;
+
+    // Set the lastPrompt variable
     $lastPrompt = $('#last-prompt');
 
-    var hash = window.location.hash.replace('#', '');
-    if (hash === '') {
-        window.location.hash = 'home';
-        hash = 'home';
-    }
-    if (hash !== 'home') {
-        window.location.hash = 'home';
-        changeDirectoryClick(hash);
-    } else {
-        listClick();
-    }
-    $body.focus();
+    // Update the currentPointer variable based on the current hash
+    goToHash();
+    // Automatically update the currentPointer variable whenever the hash changes
+    $(window).on('hashchange', updateCurrentPointer);
 
+    // Set the focus on body in order to detect keystrokes
+    $lastPrompt.focus();
     // Detect the Tab key and prevent it from starting off a focus cycle
-    $body.keydown(function (event) {
-        if (event.key === 'Tab') {
-            event.preventDefault();
-            $body.focus();
-            $body.append('<p>I feel your pain, my developer friend.</p><p>But this is not a real terminal so please type the whole thing.</p>');
-            $lastPrompt.removeAttr('id');
-            $body.append('<p class="prompt ' + hash + '" id="last-prompt">' + $lastPrompt.html());
-            $lastPrompt = $('#last-prompt');
-            $body.scrollTop($body[0].scrollHeight);
-        }
-    });
-
-    // Detect other keys and then transfer control to command processing
-    $body.keyup(function (event) {
-        var key = event.key;
-        if (key === 'Enter') {
-            process();
-        } else if (key === 'Backspace') {
-            var content = $lastPrompt.html();
-            $lastPrompt.html(content.substring(0, content.length - 1));
-        } else if (
-            (key.charCodeAt(0) >= 97 && key.charCodeAt(0) <= 122 && key.length == 1) ||  // Lowercase a-z
-            (key.charCodeAt(0) >= 65 && key.charCodeAt(0) <= 90 && key.length == 1) ||  // Uppercase A-Z
-            (key.charCodeAt(0) == 47) ||  // Slash
-            (key.charCodeAt(0) == 95) ||  // Underscore
-            (key.charCodeAt(0) == 46) ||  // Dot
-            (key.charCodeAt(0) == 32)) {  // Space
-            $lastPrompt.append(event.key);
-            $body.scrollTop($body[0].scrollHeight);
-        }
+    $lastPrompt.on('keydown', tabGrab);
+    // Detect the Enter key and prevent if from creating a new line
+    $lastPrompt.on('keydown', enterGrab);
+    // Focus snatching from body
+    $body.on('click', function (event) {
+        event.preventDefault();
+        $lastPrompt.focus();
+        cursorToEnd();
     });
 });
+
+// Helper functions
 
 function replaceAllOccurrences(str, find, replace) {
     return str.replace(new RegExp(find, 'g'), replace);
 }
 
-// TODO: Replace includes('') with /regex matching/
-function process() {
-    var hash = window.location.hash.replace('#', '');
-    var command = $lastPrompt.html();
-    var isBadCommand = false;
+function showPdf(fileName) {
+    window.open(window.location.origin + '/pdf/' + fileName);
+}
 
-    console.log('Command received: ' + command);
+function cursorToEnd() {
+    let el = document.getElementById("last-prompt");
+    let range = document.createRange();
+    let sel = window.getSelection();
+    range.selectNodeContents(el);
+    range.collapse(false);
+    sel.removeAllRanges();
+    sel.addRange(range);
+}
 
-    if (commandHeirarchy[hash].indexOf(command) === -1) {
-        $body.append('<p>Bad command. You can click the links or read <span class="yellow">help</span></p>');
-        isBadCommand = true;
-    } else {
-        if (command === 'ls') {
-            list();
-        } else if (command.includes('cd')) {
-            changeDirectory(command.replace('cd ../', '').replace('cd ', ''));
-        } else if (command.includes('ccat') || command.includes('cat')) {
-            colorizingConcatenate(command.replace('ccat ', '').replace('cat ', ''));
-        } else if (command === 'clear') {
-            clearScreen();
-        } else if (command === 'help') {
-            $body.append('<p><span class="yellow">ls</span> shows the files in the current directory</p>');
-            $body.append('<p><span class="yellow">cd </span><span class="blue"><em>dirname</em></span> lets you change directories</p>');
-            $body.append('<p><span class="yellow">ccat </span><span class="green">filename</span> shows the contents of a file</p>');
-            $body.append('<p>These commands will work in the current directory</p>');
-            commandHeirarchy[hash].forEach(function (item) {
-                $body.append('<p class="li">' + item + '</p>')
-            });
+function newPrompt(presetContent = "") {
+    let promptClass = (currentPointer === '~') ? 'home' : currentPointer;
+    $body.append('<p class="prompt ' + promptClass + '" id="last-prompt" contenteditable="true">' + presetContent);
+    $body.scrollTop($body[0].scrollHeight);
+
+    $lastPrompt.removeAttr('id');
+    $lastPrompt.removeAttr('contenteditable');
+    $lastPrompt.off('keydown');
+
+    $lastPrompt = $('#last-prompt');
+
+    $lastPrompt.focus();
+    $lastPrompt.on('keydown', tabGrab);
+    $lastPrompt.on('keydown', enterGrab);
+}
+
+function goToHash() {
+    let hash = window.location.hash.replace('#', '');
+    if (hash === '' || hash === 'undefined') {
+        hash = '~';
+    }
+    let node = nodeNamed(hash);
+    if (node !== undefined && node.type === 'folder') {
+        changeDirectoryClick(node.name);
+    }
+}
+
+function updateCurrentPointer() {
+    let hash = window.location.hash.replace('#', '');
+    if (hash === '' || hash === 'undefined') {
+        hash = '~';
+    }
+    currentPointer = hash;
+    tree.traverseBreadthFirst(function (node) {
+        if (node.name === currentPointer || node.alternativeName === currentPointer) {
+            currentNode = node;
         }
-        else if (command === 'exit') {
-            $body.append('<p>Bye! Please close the window yourself.</p>')
+    });
+}
+
+// Keyboard grabber functions
+
+function tabGrab(event) {
+    if (event.key === 'Tab') {
+        event.preventDefault();
+
+        $body.append('<p>I can understand your pain, my developer friend.</p>');
+        $body.append('<p>But this is not a real terminal, so please type the whole command.</p>');
+
+        newPrompt($lastPrompt.html());
+
+        // Set cursor at the end of the paragraph
+        cursorToEnd();
+    }
+}
+
+function enterGrab(event) {
+    if (event.key === 'Enter') {
+        event.preventDefault();
+
+        process();
+    }
+}
+
+// Tree handling functions
+
+function getStructure() {
+    $.ajax({
+        method: 'GET',
+        url: window.location.origin + '/js/structure.json',
+        async: false,
+        dataType: 'json',
+        success: [
+            function (response) {
+                basicTree = response;
+            }
+        ]
+    });
+}
+
+function generateTree() {
+    let node = new Node(basicTree.name, basicTree.alternativeName, basicTree.type);
+    tree = new Tree(node);
+    if (node.type === 'folder') {
+        populateTree(basicTree);
+    }
+    /*
+     tree.traverseDepthFirst(function (node) {
+     console.log(node.name);
+     });
+     */
+}
+
+function populateTree(basicNode) {
+    let basicChildren = basicNode.children;
+    for (let i = 0; i < basicChildren.length; i++) {
+        let basicChild = basicChildren[i];
+        let childNode = new Node(basicChild.name, basicChild.alternativeName, basicChild.type);
+        tree.add(childNode, basicNode.name, tree.traverseBreadthFirst);
+        if (basicChild.type === 'folder') {
+            populateTree(basicChild);
         }
     }
-    $lastPrompt.removeAttr('id');
-    $body.append('<p class="prompt ' + window.location.hash.replace('#', '') + '" id="last-prompt">');
-    $lastPrompt = $('#last-prompt');
-    $body.scrollTop($body[0].scrollHeight);
-    if (command.includes('cd') && !isBadCommand) {
+}
+
+function nodeNamed(nodeName) {
+    let nodeInQuestion = undefined;
+    tree.traverseBreadthFirst(function (node) {
+        if (node.name === nodeName || node.alternativeName === nodeName) {
+            nodeInQuestion = node;
+        }
+    });
+    return nodeInQuestion;
+}
+
+function absolutePathTo(node) {
+    if (node.name === '~') {
+        return '~';
+    } else {
+        return absolutePathTo(node.parent) + '/' + node.name;
+    }
+}
+
+function nodeFrom(path) {
+    let nodeInQuestion = currentNode;
+    let pathEntities = path.split('/');
+    for (let i = 0; i < pathEntities.length; i++) {
+        let entity = pathEntities[i];
+        if (entity === '~') {
+            nodeInQuestion = tree.root;
+        } else if (entity === '.') {
+            // Do nothing
+        } else if (entity === '..') {
+            nodeInQuestion = nodeInQuestion.parent;
+        } else {
+            let nextNode = undefined;
+            for (let j = 0; j < nodeInQuestion.children.length; j++) {
+                let child = nodeInQuestion.children[j];
+                if (child.name === entity || child.alternativeName === entity) {
+                    nextNode = child;
+                }
+            }
+            if (nextNode === undefined) {
+                return undefined;
+            } else {
+                nodeInQuestion = nextNode;
+            }
+        }
+    }
+    return nodeInQuestion;
+}
+
+// Command processing
+
+function process() {
+    let command = $lastPrompt.html();
+
+    let helpRe = /help/;
+    let clearRe = /clear/;
+    let exitRe = /exit/;
+    let listRe = /ls/;
+    let changeDirectoryRe = /cd\s[~]?[a-zA-Zé\/.\-_]*/;
+    let concatenateRe = /cat\s[~]?[a-zA-Zé\/.()\-_↵\s]+/;
+
+    let commandType;
+    if (helpRe.test(command)) {
+        commandType = 'help';
+    } else if (clearRe.test(command)) {
+        commandType = 'clear';
+    } else if (exitRe.test(command)) {
+        commandType = 'exit';
+    } else if (listRe.test(command)) {
+        commandType = 'list';
+    } else if (changeDirectoryRe.test(command)) {
+        commandType = 'changeDirectory';
+    } else if (concatenateRe.test(command)) {
+        commandType = 'concatenate';
+    } else {
+        commandType = 'badCommand';
+    }
+
+    switch (commandType) {
+        case 'help':
+            help();
+            break;
+        case 'clear':
+            clear();
+            break;
+        case 'exit':
+            exit();
+            break;
+        case 'list':
+            list();
+            break;
+        case 'changeDirectory':
+            changeDirectory(command.substring(3));
+            break;
+        case 'concatenate':
+            concatenate(command.substring(4));
+            break;
+        case 'badCommand':
+            badCommand();
+            break;
+    }
+
+    newPrompt();
+
+    if (commandType === 'changeDirectory') {
+        // List files after the process
         listClick();
     }
 }
 
-function listClick() {
-    $lastPrompt.typed({
-        strings: ['ls'],
-        typeSpeed: 100,
-        content: 'text',
-        callback: process
-    });
+// Individual command processors
+
+function help() {
+    $body.append('<p>If you find this hard, you may use the links to browse</p>');
+    // TODO: Show help relevant to the currentNode
+}
+
+function clear() {
+    window.location.reload(true);
+}
+
+function exit() {
+    $body.append('<p><span class="yellow">Goodbye!</span></p>');
+    $body.append('<p>JavaScript is not allowed to close this window.</p>');
 }
 
 function list() {
-    var hash = window.location.hash.replace('#', '');
-    if (hash === 'home') {
-        $body.append(
-            '<p>'
-            + '<a class="blue" onclick="changeDirectoryClick(\'info\')">info</a>&nbsp;&nbsp;'
-            + '<a class="blue" onclick="changeDirectoryClick(\'résumé\')">résumé</a>&nbsp;&nbsp;'
-            + '<a class="green" onclick="colorizingConcatenateClick(\'contact.md\')">contact.md</a>&nbsp;&nbsp;'
-            + '<a class="green" onclick="colorizingConcatenateClick(\'credits.md\')">credits.md</a>'
-            + '</p>'
-        );
-    }
-    if (hash === 'info') {
-        $body.append(
-            '<p>'
-            + '<a class="blue" onclick="changeDirectoryClick(\'..\')">.. (up)</a>&nbsp;&nbsp;'
-            + '<a class="green" onclick="colorizingConcatenateClick(\'info.md\')">info.md</a>'
-            + '</p>'
-        );
-    }
-    if (hash === 'résumé') {
-        $body.append(
-            '<p>'
-            + '<a class="blue" onclick="changeDirectoryClick(\'..\')">.. (up)</a>&nbsp;&nbsp;'
-            + '<a class="green" onclick="showPdf(\'résumé_master.pdf\')">résumé_master.pdf</a>&nbsp;&nbsp;'
-            + '<a class="green" onclick="showPdf(\'résumé_computing.pdf\')">résumé_computing.pdf</a>&nbsp;&nbsp;'
-            + '<a class="green" onclick="showPdf(\'résumé_physics.pdf\')">résumé_physics.pdf</a>'
-            + '</p>'
-        );
-        $body.append('<p><em><strong>Note:</strong> You may experience problems when trying to <span class="yellow">ccat</span> pdf files</em></p>');
-        $body.append('<p><em>In that case, click the file name and then your browser will let you proceed</em></p>');
-    }
-    if (hash === 'contact') {
-        $body.append(
-            '<p>'
-            + '<a class="blue" onclick="changeDirectoryClick(\'..\')">.. (up)</a>&nbsp;&nbsp;'
-            + '<a class="green" onclick="colorizingConcatenateClick(\'contact.md\')">contact.md</a>'
-            + '</p>'
-        )
-    }
-}
-
-function changeDirectoryClick(dir) {
-    $lastPrompt.typed({
-        strings: ['cd ' + dir],
-        typeSpeed: 100,
-        content: 'text',
-        callback: process
-    });
-}
-
-function changeDirectory(destinationHash) {
-    // Ensure that the user is on one of the known hashes
-    var hash = destinationHash;
-    if (hash === '..') {
-        hash = 'home';
-    } else if (hash === 'resume') {
-        hash = 'résumé';
-    }
-    window.location.hash = hash;
-}
-
-function colorizingConcatenateClick(file) {
-    $lastPrompt.typed({
-        strings: ['ccat ' + file],
-        typeSpeed: 100,
-        content: 'text',
-        callback: process
-    });
-}
-
-function colorizingConcatenate(fileName) {
-    // Ensure that the file requested exists
-    var allowedNames = ['info.md', 'contact.md', 'credits.md'];
-    if (allowedNames.indexOf(fileName) === -1) {
-        var resumes = [
-            'résumé_master.pdf', 'résumé_computing.pdf', 'résumé_physics.pdf',
-            'resume_master.pdf', 'resume_computing.pdf', 'resume_physics.pdf'
-        ];
-        if (resumes.indexOf(fileName) === -1) {
-            $body.append('<p>File not found. Please check the command or try <span class="yellow">help</span></p>');
+    let string = '<p>';
+    for (let i = 0; i < currentNode.children.length; i++) {
+        let child = currentNode.children[i];
+        if (child.type === 'folder') {
+            string = string + '<a class="blue" onclick="changeDirectoryClick(absolutePathTo(nodeNamed(\'' + child.name + '\')))">';
+            string = string + child.name;
+            string = string + '</a>';
         } else {
-            showPdf(replaceAllOccurrences(fileName, 'resume', 'résumé'));
+            string = string + '<a class="green" onclick="concatenateClick(absolutePathTo(nodeNamed(\'' + child.name + '\')))">';
+            string = string + child.name;
+            string = string + '</a>';
         }
-    } else {
+        string = string + "&nbsp;&nbsp;"
+    }
+    string = string + '</p>';
+    $body.append(string);
+}
+
+function changeDirectory(path) {
+    let nextNode = nodeFrom(path);
+    if (nextNode === undefined || nextNode.type === 'file') {
+        badCommand();
+        return;
+    }
+    currentNode = nextNode;
+    currentPointer = currentNode.name;
+    window.location.hash = currentPointer;
+}
+
+function concatenate(path) {
+    let node = nodeFrom(path);
+    if (node === undefined || node.type === 'folder') {
+        badCommand();
+        return;
+    }
+    let fileName = node.name;
+    if (fileName.includes('.md')) {
         $.ajax({
             url: window.location.origin + '/markdowns/' + fileName.replace('.', '') + '.html',
             method: 'GET',
@@ -238,13 +334,53 @@ function colorizingConcatenate(fileName) {
                 }
             ]
         });
+    } else if (fileName.includes('.pdf')) {
+        showPdf(replaceAllOccurrences(fileName, 'resume', 'résumé'));
     }
 }
 
-function clearScreen() {
-    window.location.reload(true);
+function badCommand() {
+    $body.append('<p>Bad command. See <a class="yellow" onclick="helpClick()">help</a> or use the links to browse.</p>');
 }
 
-function showPdf(fileName) {
-    window.open(window.location.origin + '/pdf/' + fileName);
+// Automated command typing functions
+
+function doNothing() {
+    // Do nothing
+}
+
+function listClick() {
+    $lastPrompt.typed({
+        strings: ['ls'],
+        typeSpeed: TYPE_SPEED,
+        content: 'text',
+        callback: process
+    });
+}
+
+function changeDirectoryClick(dirPath) {
+    $lastPrompt.typed({
+        strings: ['cd ' + dirPath],
+        typeSpeed: TYPE_SPEED,
+        content: 'text',
+        callback: process
+    });
+}
+
+function concatenateClick(filePath) {
+    $lastPrompt.typed({
+        strings: ['cat ' + filePath],
+        typeSpeed: TYPE_SPEED,
+        content: 'text',
+        callback: (filePath.includes('.pdf')) ? doNothing : process
+    });
+}
+
+function helpClick() {
+    $lastPrompt.typed({
+        strings: ['help'],
+        typeSpeed: TYPE_SPEED,
+        content: 'text',
+        callback: process
+    });
 }
